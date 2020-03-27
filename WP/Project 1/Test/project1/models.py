44b9a797bb8281450669004be9edc47f209bdff9
Deepak <deepakbhimavarapu@msitprogram.net>
2020-03-27 18:58:38 +0530

    pylint completed

--- WP/Project 1/Test/project1/models.py
+++ WP/Project 1/Test/project1/models.py
@@ -1,16 +1,18 @@
-from sqlalchemy import Column, String, DateTime, text
-from sqlalchemy.types import TIMESTAMP
+"""Modules is used to generate DB tables"""
 from datetime import datetime
+from sqlalchemy import Column, String
+from sqlalchemy.types import TIMESTAMP
 from sqlalchemy.ext.declarative import declarative_base
 Base = declarative_base()
 
 class User(Base):
+    """User class to generate DB tables"""
     __tablename__ = 'users'
     email = Column(String(120), primary_key=True)
     pwd = Column(String(50))
     created_at = Column(TIMESTAMP, default=datetime.utcnow, nullable=False)
-    
     def __init__(self, email, pwd):
+    	# constructor
         self.email = email
         self.pwd = pwd
 
