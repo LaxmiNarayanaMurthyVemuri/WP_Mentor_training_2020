72382a0e6e8b127abab6fb52e985073be55f2b3f
LaxmiNarayanaMurthyVemuri <murthyvemuri@msitprogram.net>
2020-03-31 14:08:06 +0530

    testcases added for auth, register, books search, books for valid and invalid and coverage has checked, verified

--- WP/Project 1/Test/project1/test_basic.py
+++ WP/Project 1/Test/project1/test_basic.py
@@ -57,6 +57,24 @@ class BasicTests(unittest.TestCase):
         response = self.app.get('/', follow_redirects=True)
         self.assertEqual(response.status_code, 200)
 
+    def test_logout(self):
+        response = self.logout()
+        self.assertEqual(response.status_code, 200)
+
+    def test_admin(self):
+        response = self.app.get("/admin")
+        self.assertEqual(response.status_code, 200)
+
+    def test_login_invalid(self):
+        response = self.login('adminmsitprogram.net', 'a1dmin')
+        self.assertEqual(response.status_code, 200)
+        self.assertIn(b'Please enter valid email', response.data)
+
+    def test_login(self):
+        response = self.login('murthy@msitprogram.net', 'a1dmin')
+        self.assertEqual(response.status_code, 200)
+        self.assertIn(b'Email is not register. Please register', response.data)
+
     def test_register(self):
         response = self.app.get('/register', follow_redirects=True)
         self.assertEqual(response.status_code, 200)
@@ -68,6 +86,11 @@ class BasicTests(unittest.TestCase):
 
     def test_invalid_user_registeremail(self):
         response = self.register('adminmsitprogram.net', 'a1dmin')
+        self.assertEqual(response.status_code, 200)
+        self.assertIn(b'Please enter valid email', response.data)
+
+    def test_invalid_user_registeremail_fun(self):
+        response = self.register('adminmsitprogramnet', 'a1dmin')
         self.assertEqual(response.status_code, 200)
         self.assertIn(b'Please enter valid email', response.data)
 
@@ -85,9 +108,27 @@ class BasicTests(unittest.TestCase):
         books = db_session.query(Book).filter(Book.year==1994)
         self.assertEqual(response.status_code, 200)
 
-    def test_invalid_search(self):
+    def test_invalid_search_year(self):
         response = self.books('year', 2020)
         books = db_session.query(Book).filter(Book.year==2020)
+        self.assertEqual(response.status_code, 200)
+        self.assertIn(b'No results found for your search', response.data)
+
+    def test_invalid_search_isbn(self):
+        response = self.books('ISBN', "2058X")
+        books = db_session.query(Book).filter(Book.isbn=="2058X")
+        self.assertEqual(response.status_code, 200)
+        self.assertIn(b'No results found for your search', response.data)
+
+    def test_invalid_search_author(self):
+        response = self.books('author', "Murthy")
+        books = db_session.query(Book).filter(Book.author=="Murthy")
+        self.assertEqual(response.status_code, 200)
+        self.assertIn(b'No results found for your search', response.data)
+
+    def test_invalid_search_name(self):
+        response = self.books('Name', "Vemuri")
+        books = db_session.query(Book).filter(Book.name=="Vemuri")
         self.assertEqual(response.status_code, 200)
         self.assertIn(b'No results found for your search', response.data)
 
